59c8509f0e1b40469665b91ed0f24714d61697ed
Tomivan <gracejunkie20@gmail.com>
2022-04-20 16:08:37 +0100

    style changes

--- src/components/customer-service/customer-service.component.jsx
+++ src/components/customer-service/customer-service.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Feature from "../../assets/images/features.png";
 import Live from "../../assets/images/live.png";
 import Whispering from "../../assets/images/whispering.png";
+import CRM from "../../assets/images/crm.png"
+import Modifications from "../../assets/images/modifications.png";
+import Monitoring from "../../assets/images/monitoring.png";
+import Queuing from "../../assets/images/queuing.png";
 import './customer-service.styles.css';
 
 const CustomerService = () => {
@@ -11,7 +14,7 @@ const CustomerService = () => {
             <p>Get these features with our cloud PBX solutions</p>
             <div className="customer-cards">
                 <div className="customer-card">
-                    <img src={Feature} alt="" className="customer-icon"/>
+                    <img src={Modifications} alt="" className="customer-icon"/>
                     <h5>Real-Time Modifications</h5>
                     <p>Toggle your system settings at any time,<br /> 
                     including adding new numbers or users<br /> in a single click.</p>
@@ -23,7 +26,7 @@ const CustomerService = () => {
                     activity, so you can shift resources and<br /> optimise productivity.</p>
                 </div>
                 <div className="customer-card">
-                    <img src={Feature} alt=""  className="customer-icon"/>
+                    <img src={Queuing} alt=""  className="customer-icon"/>
                     <h5>Call Queuing</h5>
                     <p>Give inbound callers the option to remain in<br /> 
                     queue until one of your agents is available<br /> to talk.</p>
@@ -35,13 +38,13 @@ const CustomerService = () => {
                     are on a live call so you can provide<br /> timely advice.</p>
                 </div>
                 <div className="customer-card">
-                    <img src={Feature} alt=""  className="customer-icon"/>
+                    <img src={CRM} alt=""  className="customer-icon"/>
                     <h5>CRM integration</h5>
                     <p>Native Talk integrates with your CRM<br /> and Helpdesk tools to keep all<br /> 
                     conversations connected.</p>
                 </div>
                 <div className="customer-card">
-                    <img src={Feature} alt=""  className="customer-icon"/>
+                    <img src={Monitoring} alt=""  className="customer-icon"/>
                     <h5>Call Monitoring</h5>
                     <p>Accelerate employee training and boost call<br /> quality assurance by monitoring live calls.</p>
                 </div>
